d7bc6859835275e2c80e43765d7b90cc0f492828
Julia DeFoor <juliagracedefoor@gmail.com>
2020-06-02 15:01:41 -0400

    Fix isValid, add the other comparison operators
    isValid was missing a check to see if all the numbers in the left and right
    set are valid as well.
    
    I am creating custom operators such as (.<) and (.>) for comparing two
    surreal numbers instead of defining (<) and (>). This is because if I
    define (<) and (>), Haskell will use my custom functions for behind the
    scenes operations, such as the ordering of maps and sets. I want to
    avoid this behavior for now, so that I can make changes without risk of
    breaking the program.
    
    I have renamed the lesser than or equal to function to (<==). I have
    added the greater than or equal to function (>==). I have added the
    functions for strictly lesser than (.<) and strictly greater than (.>).
    I have defined the "like" operator (===), which compares the values of
    two surreal numbers.

--- src/Surreal.hs
+++ src/Surreal.hs
@@ -1,66 +1,17 @@
-module Surreal
-( Surreal
-, isValid
-, (*&)
-, like
-, fromLists
-, fromInt
-, zero
-, one
-, minusone
-, showWith
-, printWith
-, fundamentalNames
-, integerNames
-, noNames
-) where
+module Surreal where
 
-import Data.List (intercalate)
-import Data.Maybe (isJust, fromJust)
+import           Data.List       (intercalate)
+import           Data.Maybe      (fromJust, isJust)
 
-import Data.Map.Strict (Map)
+import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
+import           Data.Set        (Set)
+import qualified Data.Set        as Set
 
-data Surreal = Surreal {left :: Set Surreal, right :: Set Surreal}
+data Surreal = Surreal {left :: Set Surreal, right :: Set Surreal} deriving (Eq, Ord)
 
 instance Show Surreal where
     show = showWith integerNames
-
-instance Eq Surreal where
-    a == b = a `like` b
-
-instance Ord Surreal where
-    a <= b = a *& b
-
--- Axiom one: Is this a valid surreal number?
-isValid :: Surreal -> Bool
-isValid a =
-    let pairings = Set.cartesianProduct (right a) (left a)
-    in not . any (uncurry (*&)) $ pairings
-
--- Axiom two: Is the first number less than or equal to the second number?
-(*&) :: Surreal -> Surreal -> Bool
-a *& b = 
-    let test1 = not . any (b *&) $ left a
-        test2 = not . any (*& a) $ right b
-    in test1 && test2
-
--- Definition: Are two numbers alike in value?
-like :: Surreal -> Surreal -> Bool
-like a b = a *& b && b *& a
-
--- Construct a surreal number from two lists
-fromLists :: [Surreal] -> [Surreal] -> Surreal
-fromLists ls rs = Surreal {left=Set.fromList ls, right=Set.fromList rs}
-
--- Construct a surreal number from an integer (large numbers are slow and may cause a stack overflow)
-fromInt :: Int -> Surreal
-fromInt 0 = zero
-fromInt a
-    | a < 0  = fromLists [] [fromInt (a+1)]
-    | a > 0  = fromLists [fromInt (a-1)] []
 
 zero :: Surreal
 zero = fromLists [] []
@@ -71,17 +22,73 @@ one = fromLists [zero] []
 minusone :: Surreal
 minusone = fromLists [] [zero]
 
+-- Axiom one: Is this a valid surreal number?
+isValid :: Surreal -> Bool
+isValid a =
+    let test1    = all isValid (left a) && all isValid (right a)
+        pairings = Set.cartesianProduct (right a) (left a)
+        test2    = not . any (uncurry (<==)) $ pairings
+    in  test1 && test2
+
+-- Axiom two: Is the first number less than or alike in value to the second number?
+(<==) :: Surreal -> Surreal -> Bool
+a <== b =
+    let test1 = not . any (b <==) $ left a
+        test2 = not . any (<== a) $ right b
+    in  test1 && test2
+
+(>==) :: Surreal -> Surreal -> Bool
+(>==) = flip (<==)
+
+(.<) :: Surreal -> Surreal -> Bool
+a .< b = a <== b && not (b <== a)
+
+(.>) :: Surreal -> Surreal -> Bool
+a .> b = b <== a && not (a <== b)
+
+-- Definition: Are two surreal numbers alike in value?
+like :: Surreal -> Surreal -> Bool
+like a b = a <== b && a >== b
+
+(===) :: Surreal -> Surreal -> Bool
+(===) = like
+
+-- Definition: What is the additive inverse of a surreal number?
+neg :: Surreal -> Surreal
+neg a = fromSets (Set.map neg (right a)) (Set.map neg (left a))
+
+-- Definition: What is the sum of two surreal numbers?
+--add :: Surreal -> Surreal -> Surreal
+--add a b =
+
+-- Construct a surreal number from two lists
+fromLists :: [Surreal] -> [Surreal] -> Surreal
+fromLists ls rs = Surreal { left = Set.fromList ls, right = Set.fromList rs }
+
+-- Construct a surreal number from two sets
+fromSets :: Set Surreal -> Set Surreal -> Surreal
+fromSets left right = Surreal { left = left, right = right }
+
+-- Construct a surreal number from an integer (large numbers are slow and may cause a stack overflow)
+fromInt :: Int -> Surreal
+fromInt 0 = zero
+fromInt a | a < 0 = fromLists [] [fromInt (a + 1)]
+          | a > 0 = fromLists [fromInt (a - 1)] []
+
 -- Create a string representation of a surreal number using names for certain values
 -- For example, "(( : ) : (( : ) : ))" might be replaced with "(0 : 1)"
 showWith :: Map Surreal String -> Surreal -> String
 showWith names a
-    | isJust name = fromJust name
-    | otherwise   = "(" ++ showSet (left a) ++ " : " ++ showSet (right a) ++ ")"
-    where name = Map.lookup a names
-          showSet = intercalate ", " . map (showWith names) . Set.toList 
+    | nameExists
+    = fromJust name
+    | otherwise
+    = "(" ++ stringFromSet (left a) ++ " : " ++ stringFromSet (right a) ++ ")"
+  where
+    name          = Map.lookup a names
+    nameExists    = isJust name
+    stringFromSet = intercalate ", " . map (showWith names) . Set.toList
 
--- Print a surreal number to the screen using names for certain values
--- For example, "(( : ) : (( : ) : ))" might be replaced with "(0 : 1)"
+-- Equivalent to showWith, but as an IO action
 printWith :: Map Surreal String -> Surreal -> IO ()
 printWith names = putStrLn . showWith names
 
@@ -91,9 +98,9 @@ fundamentalNames = Map.fromList [(zero, "0"), (one, "1"), (minusone, "-1")]
 
 -- Name substitutions for the integers between -1000 and 1000
 integerNames :: Map Surreal String
-integerNames = 
-    let nums = [-1000..1000]
-    in Map.fromList $ zip (map fromInt nums) (map show nums)
+integerNames =
+    let nums = [-1000 .. 1000]
+    in  Map.fromList $ zip (map fromInt nums) (map show nums)
 
 -- For printing with no name substitutions
 noNames :: Map Surreal String
